19dd5a1e90acbc244549db81b08160e8a0dacd72
andmohiko <andmohiko@gmail.com>
2024-12-27 01:19:41 +0900

    del: unused loading

--- apps/web/src/components/Base/Loading/index.tsx
+++
@@ -1,8 +0,0 @@
-import { LoadingOverlay } from '~/components/Base/LoadingOverlay'
-import { useLoadingContext } from '~/providers/LoadingProvider'
-
-export const Loading = () => {
-  const { isLoading } = useLoadingContext()
-
-  return <LoadingOverlay visible={isLoading} />
-}
