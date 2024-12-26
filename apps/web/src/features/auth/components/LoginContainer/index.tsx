315d65d5482d960cfc09d9cbd21178adbb45d8b4
andmohiko <andmohiko@gmail.com>
2024-12-27 01:08:37 +0900

    feat: login page

---
+++ apps/web/src/features/auth/components/LoginContainer/index.tsx
@@ -0,0 +1,22 @@
+import { FlexBox } from '~/components/Base/FlexBox'
+import { BasicButton } from '~/components/Buttons/BasicButton'
+import { DefaultLayout } from '~/components/Layouts/DefaultLayout'
+import { TitleText } from '~/components/Typography/TitleText'
+import { useFirebaseAuthContext } from '~/providers/FirebaseAuthProvider'
+
+export const LoginContainer = (): React.ReactNode => {
+  const { login } = useFirebaseAuthContext()
+  return (
+    <DefaultLayout>
+      <FlexBox gap={64} justify="flex-start" pt={120}>
+        <TitleText>ログイン</TitleText>
+        <FlexBox gap={32} height="initial">
+          <BasicButton onClick={login}>Googleで新規登録</BasicButton>
+          <BasicButton onClick={login} importance="secondary">
+            Googleでログイン
+          </BasicButton>
+        </FlexBox>
+      </FlexBox>
+    </DefaultLayout>
+  )
+}
---
+++ apps/web/src/pages/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+import { LoginContainer } from '~/features/auth/components/LoginContainer'
+
+const LoginPage: NextPage = () => {
+  return <LoginContainer />
+}
+
+export default LoginPage
