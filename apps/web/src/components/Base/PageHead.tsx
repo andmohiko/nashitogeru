98a402829fa6b8f6415e64d91a486e835e80a89d
andmohiko <andmohiko@gmail.com>
2024-12-28 00:47:47 +0900

    fix: view port scale

--- apps/web/src/components/Base/PageHead.tsx
+++ apps/web/src/components/Base/PageHead.tsx
@@ -4,6 +4,14 @@ export const PageHead = (): React.ReactElement => (
   <Head>
     <title>成し遂げる!!</title>
     <meta name="description" content="今年成し遂げることを書こう" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
+    <link rel="canonical" href="https://nashitogeru.andmohiko.dev/" />
     <link rel="icon" href="/favicon.ico" />
+
+    {/* OGP */}
+    <meta name="twitter:card" content="summary_large_image" />
   </Head>
 )
