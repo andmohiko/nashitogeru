91ae70f08398d533a0865f3d00c1afc81b2439d3
andmohiko <andmohiko@gmail.com>
2024-12-28 03:21:43 +0900

    fix: lock goal edit

--- apps/web/src/features/goal/components/GoalCard/index.tsx
+++ apps/web/src/features/goal/components/GoalCard/index.tsx
@@ -1,5 +1,7 @@
 import { RiEditCircleLine } from 'react-icons/ri'
 import type { Goal } from '@nashitogeru/common'
+import { CiLock } from 'react-icons/ci'
+import dayjs from 'dayjs'
 
 import styles from './style.module.css'
 
@@ -12,18 +14,27 @@ type Props = {
 }
 
 export const GoalCard = ({ goal, onClick }: Props): React.ReactNode => {
+  const today = new Date()
+  // 12/28~1/3の間は編集可能。もしくはcreatedAtが3日以内の場合も編集可能
+  const canEdit =
+    (dayjs(today).isAfter(dayjs('2021-12-27')) &&
+      dayjs(today).isBefore(dayjs('2022-01-03'))) ||
+    dayjs(today).isBefore(dayjs(goal.createdAt).add(3, 'day'))
   return (
     <div
       className={styles.card}
       role="button"
       tabIndex={0}
-      onClick={onClick}
-      onKeyDown={onClick}
+      onClick={canEdit ? onClick : undefined}
+      onKeyDown={canEdit ? onClick : undefined}
     >
       <BaseText size="lg" weight="bold">
         {goal.target}
       </BaseText>
-      <IconButton icon={<RiEditCircleLine size={20} />} importance="tertiary" />
+      <IconButton
+        icon={canEdit ? <RiEditCircleLine size={20} /> : <CiLock size={20} />}
+        importance="tertiary"
+      />
     </div>
   )
 }
