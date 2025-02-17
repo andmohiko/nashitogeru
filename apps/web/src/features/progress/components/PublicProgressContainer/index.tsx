bad5892c50c706464ae5e7bcf0c53ef582e916b4
andmohiko <andmohiko@gmail.com>
2025-02-17 17:00:49 +0900

    feat: update libs

---
+++ apps/web/src/features/progress/components/PublicProgressContainer/index.tsx
@@ -0,0 +1,33 @@
+import type { Goal } from '@nashitogeru/common'
+
+import styles from './style.module.css'
+
+import { GoalHeader } from '~/features/progress/components/GoalHeader'
+import { FlexBox } from '~/components/Base/FlexBox'
+import { LoadingAnimation } from '~/components/Base/Loading'
+import { useProgresses } from '~/features/progress/hooks/useProgresses'
+// import { ProgressesList } from '~/features/progress/components/ProgressesList'
+
+type Props = {
+  goal: Goal
+}
+
+export const PublicProgressContainer = ({ goal }: Props): React.ReactNode => {
+  const [, isLoadingProgresses] = useProgresses(goal.goalId)
+
+  return (
+    <FlexBox height="initial" gap={24}>
+      <div className={styles.container}>
+        <GoalHeader goal={goal} />
+        {isLoadingProgresses ? (
+          <FlexBox height="initial">
+            <LoadingAnimation />
+          </FlexBox>
+        ) : (
+          // <ProgressesList progresses={progresses} />
+          <div>ProgressesList</div>
+        )}
+      </div>
+    </FlexBox>
+  )
+}
